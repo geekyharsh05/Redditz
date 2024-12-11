af3acf857c552d52e5d72bdf548659de10e29b36
geekyharsh05 <hp424805@gmail.com>
2024-12-11 18:06:19 +0530

    feat: Add authentication route and settings page with user data retrieval

---
+++ app/settings/page.tsx
@@ -0,0 +1,39 @@
+import prisma from '@/lib/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import React from 'react'
+import { unstable_noStore as noStore } from "next/cache";
+import { redirect } from 'next/navigation';
+import SettingsForm from '../components/SettingsForm';
+
+async function getData(userId: string) {
+    noStore();
+    const data = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+        select: {
+            userName: true,
+        },
+    });
+
+    return data;
+}
+
+async function SettingsPage() {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return redirect("/api/auth/login");
+    }
+
+    const data = await getData(user.id);
+
+    return (
+        <div className="max-w-[1000px] mx-auto flex flex-col mt-4">
+            <SettingsForm username={data?.userName} />
+        </div>
+    )
+}
+
+export default SettingsPage;    
