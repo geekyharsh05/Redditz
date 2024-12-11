624588c3f7170e8df0469eda3f1f96312399553d
geekyharsh05 <hp424805@gmail.com>
2024-12-12 02:30:53 +0530

    feat: Implement settings and subreddit description forms with toast notifications for action states

--- app/components/SettingsForm.tsx
+++ app/components/SettingsForm.tsx
@@ -1,22 +1,46 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-import { useFormState } from "react-dom";
-import { useEffect } from "react";
+import { useActionState, useEffect } from "react";
 import { SubmitButton } from "./Buttons";
+import { updateUsername } from "../actions/actions";
+import { useToast } from "@/hooks/use-toast";
 
+const initialState = {
+    message: "",
+    status: "",
+};
 
 export default function SettingsForm({
     username,
 }: {
     username: string | null | undefined
 }) {
+    const [state, formAction] = useActionState(updateUsername, initialState);
+    const { toast } = useToast();
+
+    useEffect(() => {
+        if (state?.status === "success") {
+            toast({
+                title: "Succesfull",
+                description: state.message,
+            });
+        } else if (state?.status === "error") {
+            toast({
+                title: "Error",
+                description: state.message,
+                variant: "destructive",
+            });
+        }
+    }, [state, toast]);
+
+
     return (
-        <form >
+        <form action={formAction}>
             <h1 className="text-3xl font-extrabold tracking-tight">Settings</h1>
             <p className="text-muted-foreground">Manage your account settings and preferences.</p>
 
@@ -35,13 +59,14 @@ export default function SettingsForm({
                 maxLength={21}
             />
 
+            {state?.status === "error" && (
+                <p className="text-destructive mt-1">{state.message}</p>
+            )}
 
             <div className="w-full flex mt-5 gap-x-5 justify-end">
-                <Button variant="secondary" asChild type="button">
-                    <Link href="/">Cancel</Link>
-                </Button>
+                <Link href='/' className={buttonVariants({ variant: "secondary"})}>Cancel</Link>
                 <SubmitButton text="Change Username" />
             </div>
         </form>
     )
-}   
+}  
---
+++ app/components/SubDescriptionForm.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { Textarea } from "@/components/ui/textarea"
+import { SaveButton } from "./Buttons"
+import { useActionState, useEffect } from "react"
+import { useToast } from "@/hooks/use-toast"
+import { updateSubRedditDescription } from "../actions/actions"
+
+interface iProps {
+    subName: string
+    description: string | null | undefined
+}
+
+const initialState = {
+    message: "",
+    status: "",
+};
+
+function SubDescriptionForm({ description, subName }: iProps) {
+    const [state, formAction] = useActionState(updateSubRedditDescription, initialState);
+    const { toast } = useToast();
+
+    useEffect(() => {
+        if (state?.status === "success") {
+            toast({
+                title: "Succesfull",
+                description: state.message,
+            });
+        } else if (state?.status === "error") {
+            toast({
+                title: "Error",
+                description: state.message,
+                variant: "destructive",
+            });
+        }
+    }, [toast, state]);
+
+    return (
+        <form className="mt-3 flex flex-col gap-y-2" action={formAction}>
+            <input type="hidden" name="subName" value={subName} />
+            <Textarea
+                placeholder="Add a description for your subreddit"
+                maxLength={100}
+                name="description"
+                defaultValue={description ?? undefined}
+            />
+            <SaveButton />
+        </form>
+    )
+}
+
+export default SubDescriptionForm
