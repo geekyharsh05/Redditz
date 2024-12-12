192555bf4e76cbf802cfc6ed356be20d24ecf0a2
geekyharsh05 <hp424805@gmail.com>
2024-12-12 05:39:40 +0530

    feat: Revamp Home page layout with CreatePostCard and enhanced UI elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,49 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import Link from "next/link";
+import Banner from "../public/banner.png";
+import HelloImage from "../public/hero-image.png";
+import CreatePostCard from "./components/CreatePostCard";
 
 export default function Home() {
   return (
-    <div>
-      index page
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4 mb-10 px-4 sm:px-6 lg:px-8">
+      <div className="w-[65%] flex flex-col gap-y-5">
+        <CreatePostCard />
+      </div>
+
+      <div className="w-[35%]">
+        <Card>
+          <Image src={Banner} alt="Banner" />
+          <div className="p-2">
+            <div className="flex items-center">
+              <Image
+                src={HelloImage}
+                alt="Hello Image"
+                className="w-10 h-16 -mt-6"
+              />
+              <h1 className="font-medium pl-3">Home</h1>
+            </div>
+            <p className="text-sm text-muted-foreground pt-2">
+              Your Home Reddit frontpage. Come here to check in with your
+              favorite communites!
+            </p>
+
+            <Separator className="my-5" />
+
+            <div className="flex flex-col gap-y-3">
+              <Button asChild variant="secondary">
+                <Link href="/r/harsh/create">Create Post</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/r/create">Create Community</Link>
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
