12ec7480ebcc951d8e63b0c4fbc3bbeba6e252c0
geekyharsh05 <hp424805@gmail.com>
2024-12-14 02:16:43 +0530

    feat: Implement voting functionality for posts with create, update, and delete actions

--- app/actions/actions.ts
+++ app/actions/actions.ts
@@ -1,10 +1,11 @@
 "use server";
 
 import prisma from "@/lib/db";
-import { Prisma } from "@prisma/client";
+import { Prisma, TypeOfVote } from "@prisma/client";
 import { checkAuth } from "@/utils/checkAuth";
 import { redirect } from "next/navigation";
 import { JSONContent } from "@tiptap/react";
+import { revalidatePath } from "next/cache";
 
 export async function updateUsername(prevState: unknown, formData: FormData) {
     const user = await checkAuth();
@@ -129,4 +130,50 @@ export async function createPost(
   });
 
   return redirect(`/post/${data.id}`);
+}
+
+export async function handleVote(formData: FormData) {
+  const user = await checkAuth();
+
+  const postId = formData.get("postId") as string;
+  const voteDirection = formData.get("voteDirection") as TypeOfVote;
+
+  const vote = await prisma.vote.findFirst({
+    where: {
+      userId: user.id,
+      postId: postId,
+    },
+  });
+
+  if (vote) {
+    if (vote.voteType === voteDirection) {
+      await prisma.vote.delete({
+        where: {
+          id: vote.id,
+        },
+      });
+
+      return revalidatePath("/", "page");
+    } else {
+      await prisma.vote.update({
+        where: {
+          id: vote.id,
+        },
+        data: {
+          voteType: voteDirection,
+        },
+      });
+      return revalidatePath("/", "page");
+    }
+  }
+
+  await prisma.vote.create({
+    data: {
+      userId: user.id,
+      postId: postId,
+      voteType: voteDirection,
+    },
+  });
+
+  return revalidatePath("/", "page");
 }
