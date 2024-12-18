2503bd9d0dce9f713f3e1ada9c83ab1eb5381449
geekyharsh05 <hp424805@gmail.com>
2024-12-19 01:15:11 +0530

    feat: Refactor Navbar component for improved structure and add icons to authentication buttons

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
-import RedditText from "../../public/logo-name.svg";
-import redditMobile from "../../public/reddit-full.svg";
-import Redditz from "../../public/redditz.svg";
 import Image from "next/image";
+import { LogInIcon, UserPlusIcon } from "lucide-react";
 import { ThemeToggle } from "./ThemeToggle";
 import { Button } from "@/components/ui/button";
 import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
@@ -17,38 +15,48 @@ async function Navbar() {
         <nav className="h-[10vh] w-full flex items-center border-b px-5 lg:px-14 justify-between">
             <Link href={"/"} className="flex items-center gap-x-3">
                 <Image
-                    src={redditMobile}
+                    src="/reddit-full.svg"
                     alt="Reddit Mobile Logo"
+                    width={40}
+                    height={40}
                     className="h-10 w-fit"
                 />
                 <Image
-                    src={Redditz}
+                    src="/redditz.svg"
                     alt="Reddit Text"
+                    width={100}
+                    height={30}
                     className="h-9 hidden w-fit lg:block"
                 />
             </Link>
+            
             <div className="flex items-center gap-x-4">
                 <ThemeToggle />
                 {user ? (
-                    <UserDropdown email={user.email} username={`${user.given_name} ${user.family_name}`} userImage={user.picture} />
+                    <UserDropdown
+                        email={user.email}
+                        username={`${user.given_name} ${user.family_name}`}
+                        userImage={user.picture}
+                    />
                 ) : (
                     <div className="flex items-center gap-x-4">
-                        <Button variant="secondary" asChild>
+                        <Button variant="secondary" asChild className="flex items-center gap-x-2">
                             <RegisterLink>
-                                Sign Up
+                                <UserPlusIcon className="h-4 w-4" />
+                                <span>Sign Up</span>
                             </RegisterLink>
                         </Button>
-                        <Button asChild>
+                        <Button asChild className="flex items-center gap-x-2">
                             <LoginLink>
-                                Login
+                                <LogInIcon className="h-4 w-4" />
+                                <span>Login</span>
                             </LoginLink>
                         </Button>
                     </div>
-                )
-                }
+                )}
             </div>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
